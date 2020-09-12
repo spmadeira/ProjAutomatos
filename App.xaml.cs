a661c0b7f2a84bf152e8495459bab0ea62f35243
Samuel <spmadeira@edu.unifor.br>
2020-09-12 19:52:23 -0300

    Add entry point

--- App.xaml.cs
+++ App.xaml.cs
@@ -13,5 +13,10 @@ namespace proj_automatos
     /// </summary>
     public partial class App : Application
     {
+        //public static void main
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+        }
     }
 }
